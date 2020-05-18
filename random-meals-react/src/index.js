03d114365137726b6aaa994dd3b4a0ae9fcc7ec3
daniemoralesb <daniel_morales81@hotmail.com>
2020-05-17 22:42:22 -0400

    Adding Hero

--- random-meals-react/src/index.js
+++ random-meals-react/src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Hero } from "./Components/Hero";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Hero/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
